83a302b24d8ef36cc8e7c516dab92a4d3b88a3c1
orsi-k <meggyesteszta@gmail.com>
2024-01-06 19:43:43 +0100

    Add model for recipe and start building rec list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'Simply a test',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'Simply a test',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/08/chana-masala-recipe-500x500.jpg'
+    ),
+  ];
 }
