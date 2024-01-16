8964a40bf7b3985fa9fa6e52430bc7795f041e1a
orsi-k <meggyesteszta@gmail.com>
2024-01-16 22:34:29 +0100

    Add edit recipe page no content just routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,9 +11,13 @@ import { RecipeService } from '../recipes.service';
 export class RecipeListComponent {
   recipes: Recipe[] = [];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private router: Router) {}
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['recipes', 'new']);
   }
 }
